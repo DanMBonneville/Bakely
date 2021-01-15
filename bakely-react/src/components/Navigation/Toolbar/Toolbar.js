dfdd395af841cbd2dcb75742f628283983a960d5
Dan Bonneville <DanMBonneville@gmail.com>
2021-01-14 20:54:15 -0600

    Nav bar altered on authentication, user can logout

--- bakely-react/src/components/Navigation/Toolbar/Toolbar.js
+++ bakely-react/src/components/Navigation/Toolbar/Toolbar.js
@@ -12,7 +12,7 @@ const toolbar = ( props ) => (
             <Logo />
         </div>
         <nav className={classes.DesktopOnly}>
-            <NavigationItems isLoggedin={props.isLoggedIn}/>
+            <NavigationItems isLoggedIn={props.isLoggedIn}/>
         </nav>
     </header>
 );

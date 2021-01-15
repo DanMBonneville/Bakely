42140b96c9d2377fd408f9f45129071816242a1c
Dan Bonneville <DanMBonneville@gmail.com>
2021-01-14 19:49:14 -0600

    bable changes, sql accuracy, port setting

--- bakely-server/src/main/resources/data.sql
+++ bakely-server/src/main/resources/data.sql
@@ -7,5 +7,4 @@ values(1002, 'test2@gmail.com', 'Carol', 'Dancer', '55555', '6126126126', 'c');
 insert into Shefs(id, email, first_name, last_name, zip_code, phone_number, password)
 values(1003, 'test3@gmail.com', 'Eva', 'fromm', '55555', '6126126126', 'e');
 
-INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) VALUES (1000, 'test@test.com', '$2a$10$vRCQHzn0oG4TxOOabcIX1.fuax5AEhkLTnnkm5iTDRdQ23uMGmgnq','ROLE_SHEF');
-INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) VALUES (1001, 'test@test.com', '$2a$10$vRCQHzn0oG4TxOOabcIX1.fuax5AEhkLTnnkm5iTDRdQ23uMGmgnq','ROLE_SHEF');
+INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) VALUES (1004, 'test@test.com', '$2a$10$vRCQHzn0oG4TxOOabcIX1.fuax5AEhkLTnnkm5iTDRdQ23uMGmgnq','ROLE_SHEF');

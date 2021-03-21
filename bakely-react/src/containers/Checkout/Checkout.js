779e512349396efe338ec95ba5256847729d3163
lucasrappette <lrappette@consult-cts.com>
2021-03-20 20:23:44 -0500

    stripe connect button

--- bakely-react/src/containers/Checkout/Checkout.js
+++ bakely-react/src/containers/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
 import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
-import classes from './Checkout.css';
+import stripe_connect from "../../assets/images/stripeConnect.png";
 
 import * as actions from '../../store/actions/index';
 
@@ -24,10 +24,7 @@ class Checkout extends Component {
   justify="center"
 >
                 <Grid item xs={2} md={2} lg={2} xl={2} sm={2} >
-                <button
-                    className={classes.block}
-                    onClick={() => this.props.createStripeSession(this.state.user.email)}
-                >Checkout</button>
+                    <img onClick={() => this.props.createStripeSession(this.state.user.email)} src={stripe_connect} width="90%" alt="Healthy living" style={{float: "center"}}></img>
                 </Grid> 
                 </Grid>
             </Container>

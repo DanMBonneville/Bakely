0dc677be0f14475d602342437fa3a0c708b2279e
lucasrappette <lrappette@consult-cts.com>
2021-05-23 20:11:42 -0500

    Payments complete

--- bakely-react/src/store/actions/payments.js
+++ bakely-react/src/store/actions/payments.js
@@ -1,49 +1,74 @@
-import * as actionTypes from './actionTypes';
-import { db } from '../../firebase';
-import axios from 'axios';
-export const getClientSetupSecret = (uid)  => {
-        return dispatch => {
-            db.collection("stripe_customers").doc(uid).get().then(user => {
-                    dispatch(updateClientSetupSecret(user.data().setup_secret));
-                    return dispatch(paymentsLoadingComplete());
-            }).catch(error => {
-                console.log("Error in getting setup secret ", error);
-            });
-    };
+import * as actionTypes from "./actionTypes";
+import { db } from "../../firebase";
+import axios from "axios";
+export const getClientSetupSecret = (uid) => {
+  return (dispatch) => {
+    db.collection("stripe_customers")
+      .doc(uid)
+      .get()
+      .then((user) => {
+        dispatch(updateClientSetupSecret(user.data().setup_secret));
+        return dispatch(paymentsLoadingComplete());
+      })
+      .catch((error) => {
+        console.log("Error in getting setup secret ", error);
+      });
+  };
 };
 export const createNewSetupIntent = (uid) => {
-    return dispatch => {
-        axios.post('https://us-central1-bakely-server.cloudfunctions.net/api/createNewSetupIntent/',
-        { uid: uid }).then(res => {
-            if(res.success) {
-                return dispatch(getClientSetupSecret(uid));
-            }
-        }).catch(e => {
-            console.log('Error in axios post', e);
-        });
-    };
+  return (dispatch) => {
+    axios
+      .post(
+        "https://us-central1-bakely-server.cloudfunctions.net/api/createNewSetupIntent/",
+        { uid: uid }
+      )
+      .then((res) => {
+        if (res.data.success) {
+          return dispatch(getClientSetupSecret(uid));
+        }
+      })
+      .catch((e) => {
+        console.log("Error in axios post", e);
+      });
+  };
 };
 export const updateClientSetupSecret = (client_setup_secret) => {
-    return {
-        type: actionTypes.GET_CLIENT_SETUP_SECRET,
-        client_setup_secret: client_setup_secret
-    }
+  return {
+    type: actionTypes.GET_CLIENT_SETUP_SECRET,
+    client_setup_secret: client_setup_secret,
+  };
 };
 export const storePaymentMethod = (payment_method_doc) => {
-    return dispatch => {
-        db.collection('payment_methods').add(payment_method_doc).then(res => {
-            return {
-                type: actionTypes.PAYMENTS_CARD_ADD_COMPLETE
+  return (dispatch) => {
+    db.collection("payment_methods")
+      .add(payment_method_doc)
+      .then((res) => {
+        axios
+          .post(
+            "https://us-central1-bakely-server.cloudfunctions.net/api/updateAdditionalPaymentDetails/",
+            {
+              uid: payment_method_doc.uid,
+              paymentMethodId: payment_method_doc.payment_method_id,
             }
-        }).catch(error => {
-            console.log("Error in storing payment token ", error);
-        });
-    };
-}
-export const paymentsLoadingComplete = () => {
-    return {
-        type: actionTypes.PAYMENTS_LOADING_COMPLETE
-    };
+          )
+          .then((res) => {
+            if (res.data.success) {
+              return {
+                type: actionTypes.PAYMENTS_CARD_ADD_COMPLETE,
+              };
+            }
+          })
+          .catch((e) => {
+            console.log("Error in axios post", e);
+          });
+      })
+      .catch((error) => {
+        console.log("Error in storing payment token ", error);
+      });
+  };
 };
-
-
+export const paymentsLoadingComplete = () => {
+  return {
+    type: actionTypes.PAYMENTS_LOADING_COMPLETE,
+  };
+};
--- bakely-react/src/store/reducers/paymentReducer.js
+++ bakely-react/src/store/reducers/paymentReducer.js
@@ -1,29 +1,54 @@
-import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../../shared/utility';
+import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
-    client_secret: null,
-    paymentsloadingComplete: false
+  client_secret: null,
+  paymentsloadingComplete: false,
+  cardExists: false,
+  last4: null,
+  brand: null,
+  expirationYear: null,
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.GET_CLIENT_SETUP_SECRET: return updateClientSetupSecret(state, action);
-        case actionTypes.PAYMENTS_LOADING_COMPLETE: return loadingComplete(state, action);
-        default: return state;
-    }
+  switch (action.type) {
+    case actionTypes.PAYMENTS_INIT:
+      return initCardInformation(state, action);
+    case actionTypes.GET_CLIENT_SETUP_SECRET:
+      return updateClientSetupSecret(state, action);
+    case actionTypes.PAYMENTS_LOADING_COMPLETE:
+      return loadingComplete(state, action);
+    default:
+      return state;
+  }
+};
+
+const initCardInformation = (state, action) => {
+  if (action.cardExists) {
+    return updateObject(state, {
+      cardExists: true,
+      last4: action.last4,
+      brand: action.brand,
+      expirationYear: action.expirationYear
+    });
+  }
+  else {
+    return updateObject(state, {
+        cardExists: false,
+      });
+  }
 };
 
 const updateClientSetupSecret = (state, action) => {
-	return updateObject(state, { 
-		client_secret: action.client_setup_secret
-	});
-}
+  return updateObject(state, {
+    client_secret: action.client_setup_secret,
+  });
+};
 
 const loadingComplete = (state, action) => {
-	return updateObject(state, { 
-		paymentsloadingComplete: true
-	});
-}
+  return updateObject(state, {
+    paymentsloadingComplete: true,
+  });
+};
 
 export default reducer;
